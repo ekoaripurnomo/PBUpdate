87284835fac53a547971cfd040e4d59563cfb818
eko <ekoaripurnomo@gmail.com>
2016-11-16 18:35:49 +0700

    find different file

--- src/PBUpdate.java
+++ src/PBUpdate.java
@@ -1,26 +1,31 @@
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 //import java.io.IOException;
 //
 //import org.apache.commons.io.FileUtils;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PBUpdate {
 	
-	public static void listFilesForFolder(final File folder) {
-	    for (final File fileEntry : folder.listFiles()) {
-	        if (fileEntry.isDirectory()) {
-	            listFilesForFolder(fileEntry);
-	        } else {
-	            System.out.println(fileEntry.getName());
-	        }
-	    }
-	}
-
-	
-	
-
 	public static void main(String[] args) {
-		final File folder = new File("D:\\sources");
-		listFilesForFolder(folder);
+		File folders = new File("D:\\sources");
+		File foldert = new File("D:\\targets");
+		File[] fileEntrys = folders.listFiles();
+		File[] fileEntryt = foldert.listFiles();
+		List lsts = new LinkedList();
+		List lstt = new LinkedList();
+		
+		for(File file :fileEntrys)lsts.add(file.getName());
+		for(File file :fileEntryt)lstt.add(file.getName());
+	
+		Collection<String> mySets = lsts;
+		Collection<String> mySett = lstt;
+		
+		
+		compareList(mySets,mySett);
 		
 //		File source = new File("D:\\sources\\move1.txt");
 //		File dest = new File("D:\\targets\\move1.txt");
@@ -31,6 +36,19 @@ public class PBUpdate {
 //		    e.printStackTrace();
 //		}
 
+	}
+	public static void compareList(Collection<String> listOne,Collection<String> listTwo){
+		Collection<String> similar = new HashSet<String>( listOne );
+		Collection<String> different = new HashSet<String>();
+		different.addAll( listOne );
+		different.addAll( listTwo );
+		
+		similar.retainAll( listTwo );
+		different.removeAll( similar );
+		
+		
+		System.out.printf("sources :%s%nTargets:%s%nSimilar:%s%nDifferent:%s%n", listOne, listTwo, similar, different);
+		
 	}
 
 }
