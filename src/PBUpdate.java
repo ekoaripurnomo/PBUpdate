0b0637de078f9936251516a54ed59541a750ecc9
eko <ekoaripurnomo@gmail.com>
2016-11-16 14:51:05 +0700

    dev java

--- src/PBUpdate.java
+++ src/PBUpdate.java
@@ -1,8 +1,35 @@
+import java.io.File;
+//import java.io.IOException;
+//
+//import org.apache.commons.io.FileUtils;
 
 public class PBUpdate {
+	
+	public static void listFilesForFolder(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	            listFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	        }
+	    }
+	}
+
+	
+	
 
 	public static void main(String[] args) {
+		final File folder = new File("D:\\sources");
+		listFilesForFolder(folder);
 		
+//		File source = new File("D:\\sources\\move1.txt");
+//		File dest = new File("D:\\targets\\move1.txt");
+//				
+//		try {
+//			FileUtils.copyFile(source, dest);
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
 
 	}
 
